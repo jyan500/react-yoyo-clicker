98b8609911eba96f312413efe99d31167f53cb6c
jyan500 <jyan500@gmail.com>
2023-12-31 17:52:20 -0800

    editing hamburger button SVG so all three lines are equal in length

--- src/components/HamburgerButton/HamburgerButton.jsx
+++ src/components/HamburgerButton/HamburgerButton.jsx
@@ -4,9 +4,15 @@ import React from 'react';
 export const HamburgerButton = ({ onClick }) => {
   return (
     <button onClick={onClick} className="p-4 text-black focus:outline-none">
-      <svg className="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7"></path>
-      </svg>
+	  <svg className = "w-8 h-8" viewBox="0 0 24 24" fill="none" stroke="currentColor" xmlns="http://www.w3.org/2000/svg">
+		  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+		  <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+		  <g id="SVGRepo_iconCarrier"> 
+			  <path d="M4 18L20 18" stroke="#000000" stroke-width="2" stroke-linecap="round"></path> 
+			  <path d="M4 12L20 12" stroke="#000000" stroke-width="2" stroke-linecap="round"></path> 
+			  <path d="M4 6L20 6" stroke="#000000" stroke-width="2" stroke-linecap="round"></path> 
+		  </g>
+	  </svg>
     </button>
   );
 };
