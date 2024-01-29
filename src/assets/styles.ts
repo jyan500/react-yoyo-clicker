54d602a918b2a4f590ed74926b91d2b45edf1056
jyan500 <jyan500@gmail.com>
2024-01-29 14:20:28 -0800

    fixed small styling bug when focusing on the custom key bindings

--- src/assets/styles.ts
+++ src/assets/styles.ts
@@ -36,7 +36,7 @@ export const borderVariants: Record<string, string> = {
 export const styles: Record<string, string> = {
 	"button" : "mr-2 shadow focus:shadow-outline focus:outline-none font-bold py-2 px-4 rounded",
 	"textInput": `bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 leading-tight focus:outline-none focus:bg-white ${textFocus("blue")}`,
-	"textInputDark": `appearance-none border-2 border-gray-200 text-gray-800 rounded w-full py-2 px-4 leading-tight ${textFocus("blue")}`,
+	"textInputDark": `appearance-none border-2 border-gray-200 text-gray-800 rounded w-full py-2 px-4 focus:outline-none leading-tight ${textFocus("blue")}`,
 	"label": "block font-bold mb-2",
 	"verticalLabel": "block font-bold",
 	"checkbox": "w-4 h-4 bg-gray-100 border-gray-300 rounded dark:bg-gray-700 dark:border-gray-600",
